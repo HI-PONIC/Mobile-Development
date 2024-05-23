b214c32c533fa1c81ba469567cdfaa8aa8ec46a8
Naufalara <naufal.arya356@gmail.com>
2024-05-23 15:56:56 +0700

    Uji coba activity signup dan activity login

--- app/src/main/java/com/example/hi_ponic/MainActivity.kt
+++ app/src/main/java/com/example/hi_ponic/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.hi_ponic
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hi_ponic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //UJI COBA
+
+        binding.btnlogin.setOnClickListener {
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnsignup.setOnClickListener {
+            intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
     }
 }
