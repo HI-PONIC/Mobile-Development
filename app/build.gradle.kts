3b8f2552d105fe59371c73e6daa0c94346e1a59d
Naufalara <naufal.arya356@gmail.com>
2024-05-24 10:18:53 +0700

    fixing depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.compose.preview.renderer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
