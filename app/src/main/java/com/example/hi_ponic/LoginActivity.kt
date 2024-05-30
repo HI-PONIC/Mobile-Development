a25e80843ea2ad27026ff5c6a044136afe85dcf2
Naufalara <naufal.arya356@gmail.com>
2024-05-30 15:58:16 +0700

    add keyboard adjuster when on text field

--- app/src/main/java/com/example/hi_ponic/LoginActivity.kt
+++ app/src/main/java/com/example/hi_ponic/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.hi_ponic
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.ViewTreeObserver
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hi_ponic.databinding.ActivityLoginBinding
+import com.example.hi_ponic.databinding.ActivityMainBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,25 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        keyboardAdjuster()
+    }
+
+    private fun keyboardAdjuster() {
+        binding.scrollView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                val r = Rect()
+                binding.scrollView.getWindowVisibleDisplayFrame(r)
+                val screenHeight: Int = binding.scrollView.rootView.height
+                val keypadHeight: Int = screenHeight - r.bottom
+                if (keypadHeight > screenHeight * 0.15) { // jika keyboard terlihat
+                    binding.scrollView.post {
+                        binding.scrollView.smoothScrollTo(0, binding.scrollView.bottom)
+                    }
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/example/hi_ponic/SignupActivity.kt
+++ app/src/main/java/com/example/hi_ponic/SignupActivity.kt
@@ -1,12 +1,18 @@
 package com.example.hi_ponic
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.ViewTreeObserver
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hi_ponic.databinding.ActivityDetailHydroponicStatisticBinding
+import com.example.hi_ponic.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,25 @@ class SignupActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        keyboardAdjuster()
+    }
+
+    private fun keyboardAdjuster() {
+        binding.scrollView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                val r = Rect()
+                binding.scrollView.getWindowVisibleDisplayFrame(r)
+                val screenHeight: Int = binding.scrollView.rootView.height
+                val keypadHeight: Int = screenHeight - r.bottom
+                if (keypadHeight > screenHeight * 0.15) { // jika keyboard terlihat
+                    binding.scrollView.post {
+                        binding.scrollView.smoothScrollTo(0, binding.scrollView.bottom)
+                    }
+                }
+            }
+        })
     }
 }
