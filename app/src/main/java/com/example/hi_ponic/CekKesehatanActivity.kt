e95887f1c57e695ded7f5c7e4ca97695dfe1785d
Naufalara <naufal.arya356@gmail.com>
2024-05-30 15:58:49 +0700

    add prediksi panen and cek kesehatan

---
+++ app/src/main/java/com/example/hi_ponic/CekKesehatanActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hi_ponic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CekKesehatanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cek_kesehatan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
