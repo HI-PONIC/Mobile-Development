af7a7df392905c5e06f226156965cfe788d8290f
Naufalara <naufal.arya356@gmail.com>
2024-06-01 13:53:31 +0700

    add mainviewmodel

---
+++ app/src/main/java/com/example/hi_ponic/view/mainView/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.hi_ponic.view.mainView
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.hi_ponic.data.UserRepository
+import com.example.hi_ponic.data.pref.UserModel
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val repository: UserRepository) : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    fun getSession(): LiveData<UserModel> {
+        return repository.getSession().asLiveData()
+    }
+
+    fun logout() {
+        viewModelScope.launch {
+            repository.logout()
+        }
+    }
+}
