3b6f3ba03a5e972452fe7eea5b9d3cf08032c06f
Naufalara <naufal.arya356@gmail.com>
2024-06-09 21:06:39 +0700

    delete PrediksiPanenActivity.kt add feature open camera and gallery in CekKesehatanActivity

--- app/src/main/java/com/example/hi_ponic/view/monitoring/CekKesehatanActivity.kt
+++ app/src/main/java/com/example/hi_ponic/view/monitoring/CekKesehatanActivity.kt
@@ -1,13 +1,31 @@
+// File: /com/example/hi_ponic/view/monitoring/CekKesehatanActivity.kt
+
 package com.example.hi_ponic.view.monitoring
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hi_ponic.R
+import com.example.hi_ponic.databinding.ActivityCekKesehatanBinding
 
 class CekKesehatanActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCekKesehatanBinding
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_IMAGE_GALLERY = 2
+    private var currentImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +34,91 @@ class CekKesehatanActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivityCekKesehatanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        openGalleryorCamera()
+    }
+
+    private fun openGalleryorCamera() {
+        binding.ivDetail.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE),
+                    0
+                )
+            } else {
+                showImageSourceOptions()
+            }
+        }
+    }
+
+    private fun showImageSourceOptions() {
+        val options = arrayOf<CharSequence>("Camera", "Gallery")
+        val builder: androidx.appcompat.app.AlertDialog.Builder =
+            androidx.appcompat.app.AlertDialog.Builder(this)
+        builder.setTitle("Select Option")
+        builder.setItems(options) { dialog, item ->
+            when {
+                options[item] == "Camera" -> {
+                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+
+                options[item] == "Gallery" -> {
+                    val pickPhoto =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(pickPhoto, REQUEST_IMAGE_GALLERY)
+                }
+            }
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                showImageSourceOptions()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    binding.ivDetail.setImageBitmap(imageBitmap)
+                }
+
+                REQUEST_IMAGE_GALLERY -> {
+                    val selectedImage: Uri? = data?.data
+                    selectedImage?.let {
+                        currentImageUri = it
+                        binding.ivDetail.setImageURI(it)
+                    }
+                }
+            }
         }
     }
 }
