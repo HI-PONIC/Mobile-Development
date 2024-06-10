1d7d8847816b39e2ca99e48dda16333868cce9f7
Elvindo <mochammadelvindo26@gmail.com>
2024-06-10 09:23:23 +0700

    - perbaiki xml dan ui = tata letak dan lock materia - bisa uplod image di get lahan - binding beberapa tombol dan acitivity

--- app/src/main/java/com/example/hi_ponic/view/monitoring/TambahLahanActivity.kt
+++ app/src/main/java/com/example/hi_ponic/view/monitoring/TambahLahanActivity.kt
@@ -1,14 +1,26 @@
 package com.example.hi_ponic.view.monitoring
 
+import android.Manifest
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hi_ponic.R
 import com.example.hi_ponic.databinding.ActivityTambahLahanBinding
-import java.util.Calendar
+import java.util.*
 
 class TambahLahanActivity : AppCompatActivity() {
 
@@ -28,6 +40,11 @@ class TambahLahanActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         dateInput()
+
+        binding.ivDetail.setOnClickListener {
+            // Panggil method untuk memilih gambar dari galeri atau kamera
+            pickImage()
+        }
     }
 
     private fun dateInput() {
@@ -43,6 +60,48 @@ class TambahLahanActivity : AppCompatActivity() {
 
         binding.edittextTanggalLayout.setEndIconOnClickListener {
             datePickerDialog.show()
+        }
+    }
+
+    private fun pickImage() {
+        val options = arrayOf<CharSequence>("Ambil Foto", "Pilih dari Galeri", "Batal")
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Pilih Sumber Gambar")
+        builder.setItems(options) { dialog, item ->
+            when {
+                options[item] == "Ambil Foto" -> {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
+                    } else {
+                        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(takePicture, 0) // 0 untuk kode permintaan
+                    }
+                }
+                options[item] == "Pilih dari Galeri" -> {
+                    val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(pickPhoto, 1) // 1 untuk kode permintaan
+                }
+                options[item] == "Batal" -> {
+                    dialog.dismiss()
+                }
+            }
+        }
+        builder.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                0 -> {
+                    val selectedImage: Bitmap = data!!.extras!!.get("data") as Bitmap
+                    binding.ivDetail.setImageBitmap(selectedImage)
+                }
+                1 -> {
+                    val selectedImage: Uri? = data!!.data
+                    binding.ivDetail.setImageURI(selectedImage)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/hi_ponic/view/profile/edit_username.kt
+++ app/src/main/java/com/example/hi_ponic/view/profile/edit_username.kt
@@ -1,6 +1,8 @@
 package com.example.hi_ponic.view.profile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,13 @@ class edit_username : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Menambahkan onClickListener untuk tombol ganti password
+        findViewById<Button>(R.id.gantipasword).setOnClickListener {
+            // Navigasi ke activity Ganti Password
+            val intent = Intent(this, change_password::class.java)
+            startActivity(intent)
         }
     }
 }
